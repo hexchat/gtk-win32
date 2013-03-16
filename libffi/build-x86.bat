80887b2edb2eca89253e089fe1b823869383aa8b
Berke Viktor <bviktor@hexchat.org>
2013-03-16 03:28:22 +0100

    libffi disable opt

--- libffi/build-x86.bat
+++ libffi/build-x86.bat
@@ -1,6 +1,6 @@
 #run this from mozilla-build\start-msvc11.bat
 #cd /c/mozilla-build/hexchat/libffi-3.0.13
 
-./configure CC=$(pwd)/msvcc.sh LD=link CPP='cl -nologo -EP' CFLAGS='-O2' --build=i686-pc-mingw32
+./configure CC=$(pwd)/msvcc.sh LD=link CPP='cl -nologo -EP' CFLAGS='-Od' --build=i686-pc-mingw32
 make clean
 make
