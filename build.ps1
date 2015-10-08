c4a57f2c65c8c8fdc804c7575c8198ac0e684514
Ignacio Casal Quinteiro <ignacio.casal@nice-software.com>
2015-10-08 14:12:39 +0200

    libffi: patch to fix the build for c89

--- build.ps1
+++ build.ps1
@@ -547,6 +547,7 @@ $items['libffi'].BuildScript = {
 
 	Exec $patch -p1 -i libffi-msvc-complex.patch
 	Exec $patch -p1 -i libffi-win64-jmp.patch
+	Exec $patch -p1 -i 0001-Fix-build-on-windows.patch
 
 	switch ($filenameArch) {
 		'x86' {
